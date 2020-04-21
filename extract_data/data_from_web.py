6147262eba35d37b3e7dfe11b574ff71cc0bde2d
mic <mchechik@aol.com>
2020-04-21 16:32:23 -0400

    impl path enumeration

---
+++ extract_data/data_from_web.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+import requests
+import sys
+from bs4 import BeautifulSoup
+
+# Base URL
+URL = 'https://lehd.ces.census.gov/data'
+
+# TODO Expand paths map to include older versions
+data_source_paths = {
+    'lodes': 'lodes/LODES7',
+    'j2j': 'j2j/latest_release',
+    'qwi': 'qwi/latest_release'
+}
+
+# Rate limit client to not crash server, in effect finding the most number of data files
+# in MAX_REQUESTS number of requests
+MAX_REQUESTS = 20
+curr_requests = 0
+  
+try:
+    data_file_collector = []
+    if len(sys.argv) != 2:
+        raise Exception('usage')
+    elif sys.argv[1] == 'all':
+        # Retrieve all supported data sources
+        print('Retrieving all data is not yet supported. Please specify a data source.')
+    else:
+        # Validate source name, get data if source is valid
+        if sys.argv[1] in data_source_paths:
+            URL = URL + '/' + data_source_paths[sys.argv[1]] + '/'
+
+            # Recurse down path to find all csv files with data
+            def get_csv_data(URL):
+                global curr_requests
+                resp = requests.get(url = URL)
+                soup = BeautifulSoup(resp.content, features='html.parser')
+                for link in soup.findAll('a'):
+                    # Validate path is unique and not visited already
+                    curr_link = link.get('href')
+                    if (curr_link not in URL) and ('/' in curr_link) and (curr_requests < MAX_REQUESTS):
+                        curr_requests += 1
+                        get_csv_data(URL + curr_link)
+                    elif 'csv' in curr_link:
+                        data_file_collector.append(URL + curr_link)
+            get_csv_data(URL)
+            print(data_file_collector)
+        else:
+            raise Exception('unsupported')
+
+# Handle exceptions
+except Exception as e:
+    err = 'Unknown error'
+    if str(e) == 'usage':
+        err = 'Unknown usage error\nUsage: data_from_web.py <source name>'
+        if len(sys.argv) > 2:
+            err = 'Too many args'
+        elif len(sys.argv) < 2:
+            err = 'No data source name provided'
+    elif str(e) == 'unsupported':
+        err = 'Unsupported data source selected'
+
+    print('Error: {:s}\nCaused by: {:s}'.format(err, str(e)))
