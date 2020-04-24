56b2c41398ae4eb1624c3051c46bed5098f0387d
mic <mchechik@aol.com>
2020-04-24 17:10:13 -0400

    data load attempt

--- extract_data/data_from_web.py
+++ extract_data/data_from_web.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import requests
 import sys
+import os
 from bs4 import BeautifulSoup
 
 # Base URL
@@ -15,10 +16,10 @@ data_source_paths = {
     'qwi': 'qwi/latest_release'
 }
 
-# Rate limit client to not crash server, in effect finding the most number of data files
-# in MAX_REQUESTS number of requests
-MAX_REQUESTS = 1
-curr_requests = 0
+# Rate limit client to not crash server, in effect finding all number of data files
+# in at URL depth equal to MAX_DEPTH
+MAX_DEPTH = 2
+curr_depth = 0
 
 try:
     data_file_collector = []
@@ -34,14 +35,14 @@ try:
 
             # Recurse down path to find all csv files with data
             def get_csv_data(URL):
-                global curr_requests
+                global curr_depth
                 resp = requests.get(url = URL)
                 soup = BeautifulSoup(resp.content, features='html.parser')
                 for link in soup.findAll('a'):
                     # Validate path is unique and not visited already
                     curr_link = link.get('href')
-                    if (curr_link not in URL) and ('/' in curr_link) and (curr_requests < MAX_REQUESTS):
-                        curr_requests += 1
+                    if (curr_link not in URL) and ('/' in curr_link) and (curr_depth < MAX_DEPTH):
+                        curr_depth += 1
                         get_csv_data(URL + curr_link)
                     elif 'csv' in curr_link:
                         data_file_collector.append(URL + curr_link)
@@ -53,12 +54,20 @@ try:
                 file_resp = requests.get(url)
                 if file_resp.status_code == 200:
                     print('Saving file from {:s} to directory {:s}'.format(url, DATA_DIR))
-                    fs = open(DATA_DIR + '/' + url.split('/')[-1], 'wb')
-                    fs.write(file_resp.content)
-                    fs.close()
+                    
+                    split_url = url.split('/')
+                    filepath = (DATA_DIR + '/' 
+                        + data_source_paths[sys.argv[1]] + '/' 
+                        + ('/' if split_url[-3] in data_source_paths[sys.argv[1]] else split_url[-3] + '/')
+                        + split_url[-2] + '/'
+                        + split_url[-1]
+                    )
+
+                    os.makedirs(os.path.dirname(filepath), exist_ok = True)
+                    with open(filepath, 'wb') as fs:
+                        fs.write(file_resp.content)
                 else:
                     print('Error downloading {:s}\nReceived non-200 status code.'.format(url))
-
         else:
             raise Exception('unsupported')
 
